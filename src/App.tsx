30c5384b9ed754e028711518ecd83ab884bfc150
sumon-hossain <sumonengr.96@gmail.com>
2022-12-22 00:22:12 +0600

    Redux simply Done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import logo from "./assets/react.svg";
+import "./App.css";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  reset,
+  selectCount,
+} from "../counter/CounterSlice";
+import styles from "./Counter.module.css";
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
+        >
+          Add Amount
+        </button>
+        <button className={styles.button} onClick={() => dispatch(reset())}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
